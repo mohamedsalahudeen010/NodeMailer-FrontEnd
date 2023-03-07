5bf356903d8a26b368e678f7f2d2626394ee5134
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-03-07 23:43:16 +0530

    Update forgetPassword.js

--- src/components/forgetPassword.js
+++ src/components/forgetPassword.js
@@ -37,7 +37,7 @@ const ForgetPage=()=>{
               console.log("error",error)
             }
       
-            
+             history.push("/") 
           }
    return(
    <div className="container">   
